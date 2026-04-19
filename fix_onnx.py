83789936d3a1efd635911998c3574cbee9ee0e59
sharadpolackalsunil <sharadpolackalsunil2006@gmail.com>
2026-04-19 16:30:04 +0530

    fix

--- fix_onnx.py
+++ fix_onnx.py
@@ -6,6 +6,7 @@ model.export(
     format="onnx",
     opset=11,             # This is the "Magic Number" for Jetson Nano
     dynamic=False,        # Forces a fixed 640x640 size (Nano requirement)
+    batch=16,             # MUST BE 16 to match PGIE batch-size config!
     imgsz=[640, 640],     
     simplify=True         # Clean up unnecessary PC-only layers
 )
